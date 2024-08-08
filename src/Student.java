f4e9bdb037dfc24cd5ad279918a42b456dc316e1
Suma G <suma.siriyan@gmail.com>
2024-08-09 02:01:59 +0530

    delete

---
+++ src/Student.java
@@ -0,0 +1,8 @@
+public class Student {
+   static void callStudent() {
+
+        /*System.out.println("sStudent name : " + stdName);
+        System.out.println("Student ID : " + stdId);
+        System.out.println("Student section : " + stdSection);*/
+    }
+}
