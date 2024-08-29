78e1f1409e30f4db57a26030f8d67cb4a60c1962
Suma G <suma.siriyan@gmail.com>
2024-08-29 23:53:53 +0530

    session01 assignment

---
+++ src/main/java/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    int stdId;
+    String stdName;
+    String stdSection;
+
+    static void callStudent() {
+        System.out.println("Student method called");
+    }
+}
