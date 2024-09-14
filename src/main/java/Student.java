444d0d56994d84f8ea65fd16af0ca1b8924ae48f
Sumagpoojari <suma.siriyan@gmail.com>
2024-09-14 19:04:29 +0530

    Delete src/main/java directory

--- src/main/java/Student.java
+++
@@ -1,9 +0,0 @@
-public class Student {
-    int stdId;
-    String stdName;
-    String stdSection;
-
-    static void callStudent() {
-        System.out.println("Student method called");
-    }
-}
